e1b5cdb0c3106f1697d845590cb115570e85d781
rom <rom>
2008-12-15 18:28:04 +0000

    oops, wrong Z

--- HAPPEX/hamcTgtHAPPEX.C
+++ HAPPEX/hamcTgtHAPPEX.C
@@ -31,15 +31,15 @@ Int_t hamcTgtHAPPEX::Init(hamcExpt *expt) {
   components.push_back(new hamcTgtSlab(
      "aluminum", 0, 0.0014, 0.0014, 0.089, 14, 13, 25.3, 2.7));
   components.push_back(new hamcTgtSlab(
-     "hydrogen", 1, 0.2, 0.2, 8.66, 1, 0, 0.938, 0.0708));
+     "hydrogen", 1, 0.2, 0.2, 8.66, 1, 1, 0.938, 0.0708));
   components.push_back(new hamcTgtSlab(
      "aluminum", 2, 0.00178, 0.00178, 0.089, 14, 13, 25.3, 2.7));
 
-  // FIXME : may move this to base class
   expt->inout->AddToNtuple("zscat",&zscatt);
-
+ 
   hamcTarget::Setup();
   hamcTarget::Print();
+
 
   did_init = kTRUE;
 
