1d18d42e2cd51c9fdb89eb194490d26ef510b224
rom <rom>
2009-11-25 13:26:29 +0000

    add Ca48 and Sn120 target options

--- PREX/hamcTgtPREX.C
+++ PREX/hamcTgtPREX.C
@@ -4,6 +4,7 @@
 #include "hamcTgtPREX.h"
 #include "hamcExpt.h"
 #include "hamcInout.h"
+#include "THaString.h"
 #include "Rtypes.h"
 #include "TRandom.h"
 #include <string>
@@ -28,14 +29,40 @@ Int_t hamcTgtPREX::Init(hamcExpt *expt) {
 
   if (did_init) return OK;
 
+  THaString strin;
+  vector<string> sdata; 
+  sdata = expt->inout->GetStrVect("PREX_model");
+  Int_t islead=1;
+  if (sdata.size()>=1) {
+    strin = sdata[0];
+    if (strin.CmpNoCase("horca")==0) {
+      cout << "Using a Calcium Target "<<endl;
+      // need to check RL and density
+     components.push_back(new hamcTgtSlab(
+	  // 10% of RL = 0.66; Teff = 0.37*T (RL loss)
+       "calcium", 0, 0.0066, 0.0024, 6.6, 48, 20, 45, 1.6));
+     islead=0;
+    }
+    if (strin.CmpNoCase("horsn")==0) {
+      cout << "Using a Tin Target "<<endl;
+      // need to check RL and density
+     components.push_back(new hamcTgtSlab(
+       "tin", 0, 0.0016, 0.00059, 1.6, 120, 50, 112, 5.8));
+     islead=0;
+    }
+  }
+
   // Tgt_eff = 0.37 * Tgt_Len, accounts for radiative tail
   // not already in hamc event generation.
-  components.push_back(new hamcTgtSlab(
-     "diamond", 0, 0.00015, 0.000056, 0.0188, 12, 6, 11.25, 3.52));
-  components.push_back(new hamcTgtSlab(
-     "lead",    1, 0.00050, 0.000186, 0.0056, 208, 82, 195, 11.35));
-  components.push_back(new hamcTgtSlab(
-     "diamond", 0, 0.00015, 0.000056, 0.0188, 12, 6, 11.25, 3.52));
+  
+  if (islead) {
+    components.push_back(new hamcTgtSlab(
+      "diamond", 0, 0.00015, 0.000056, 0.0188, 12, 6, 11.25, 3.52));
+    components.push_back(new hamcTgtSlab(
+      "lead",    1, 0.00050, 0.000186, 0.0056, 208, 82, 195, 11.35));
+    components.push_back(new hamcTgtSlab(
+      "diamond", 0, 0.00015, 0.000056, 0.0188, 12, 6, 11.25, 3.52));
+  }
 
   expt->inout->AddToNtuple("zscat",&zscatt);
 
