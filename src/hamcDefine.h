0db24f7d69389ace4329c844da294893f80204ac
rom <rom>
2010-09-01 16:26:30 +0000

    new Define header

---
+++ src/hamcDefine.h
@@ -0,0 +1,11 @@
+#define IELECTRON 10
+#define IPROTON   11
+#define IPION     12
+#define melectron 0.000511
+#define mpion     0.140
+#define mproton   0.938
+#define PI        3.1415926
+
+#define OK     1
+#define ERROR -1
+
